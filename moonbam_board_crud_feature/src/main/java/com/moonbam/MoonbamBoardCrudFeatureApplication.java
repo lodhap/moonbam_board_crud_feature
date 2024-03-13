a704a8cc81ded9afb7cf3bb707b12be2330aa63a
bsj4387 <7834jsb@gmail.com>
2024-03-13 17:43:41 +0900

    init

---
+++ moonbam_board_crud_feature/src/main/java/com/moonbam/MoonbamBoardCrudFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.moonbam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonbamBoardCrudFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonbamBoardCrudFeatureApplication.class, args);
+	}
+
+}
